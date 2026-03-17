88dc6d135f4a6526501eb752e27d1ca8f207dc89
Eltik <xp10d363@gmail.com>
2026-03-17 01:04:37 -0700

    fix: Scripts, docker

--- assets/download.sh
+++ assets/download.sh
@@ -4,4 +4,4 @@ set -e
 SERVER=${1:-en}
 THREADS=${2:-4}
 
-arknights-downloader --server "$SERVER" --savedir /data/ArkAssets --threads "$THREADS"
+downloader --server "$SERVER" --savedir /data/ArkAssets --threads "$THREADS" download --all
